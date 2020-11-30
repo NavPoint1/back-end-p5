e2f3acc8c027e3e82f7cd82d710ad1da42eafb39
NavPoint1 <foleym156@gmail.com>
2020-11-30 16:42:31 -0500

    guide edit functionality

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -62,10 +62,55 @@ class GuidesController < ApplicationController
     end
 
     def update
-        # maybe need to find by id if title is being edited?
-        @guide = Guide.find_by(title: params[:guide][:title])
-        # edit details
-        # render json response
+        # find by id so title can change
+        @guide = Guide.find(params[:id])
+        # validate again
+        if params[:slides].size == 0
+            render json: ["Guide must have slides"].to_json
+        elsif params[:thumbnail].size == 0
+            render json: ["Guide must have a thumbnail"].to_json
+        else
+            # check if thumbnail is new
+            if !params[:thumbnail].include?('/rails/active_storage/')
+                begin
+                    file = open(params[:thumbnail])
+                    uri = URI.parse(params[:thumbnail])
+                    filename = File.basename(uri.path)
+                rescue StandardError => e
+                    render json: ["Thumbnail failed to parse"].to_json and return
+                end
+                if file
+                    @guide.thumbnail.attach(io: file, filename: filename)
+                end
+            end
+            # update guide
+            @guide.update({
+                title: params[:title],
+                user_id: params[:user_id],
+            })
+            if @guide.save
+                # clear all old slides
+                @guide.slides.destroy_all
+                # rebuild slides
+                params[:slides].each { |slide|
+                    Slide.create({
+                        guide_id: @guide.id,
+                        header: slide[:header],
+                        content: slide[:content],
+                        media: slide[:media]
+                    })
+                }
+                # upon success... render json response 
+                render json: @guide.to_json(include: [:user, :likes, :slides], methods: :thumbnail_url)
+            else 
+                # upon failure... render json response 
+                if @guide.errors
+                    render json: @guide.errors.full_messages.to_json
+                else
+                    render json: ["Something happened."].to_json
+                end
+            end
+        end
     end
 
     def destroy
